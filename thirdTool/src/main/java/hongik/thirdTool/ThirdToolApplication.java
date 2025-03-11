d53be0dc27dff5a1d52d3cabc9de4ae10d52212b
junseong Kim <rlawnstj90@naver.com>
2025-03-11 18:36:42 +0900

    #1-기본 환경 세팅 완료했습니다!! 바로 main 커밋 1차 하겠씁니다.

---
+++ thirdTool/src/main/java/hongik/thirdTool/ThirdToolApplication.java
@@ -0,0 +1,13 @@
+package hongik.thirdTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdToolApplication.class, args);
+	}
+
+}
---
+++ thirdTool/src/test/java/hongik/thirdTool/ThirdToolApplicationTests.java
@@ -0,0 +1,13 @@
+package hongik.thirdTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
